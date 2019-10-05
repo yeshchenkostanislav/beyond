131f35403e8c8edf42e91235b42bbd8f9a9276e2
Stanislav Yeshchenko <yeshchenkostanislav@gmail.com>
2019-10-05 17:58:11 +0200

    доработал меню

--- app/elements/common-elements/header/header.js
+++ app/elements/common-elements/header/header.js
@@ -1,5 +1,16 @@
 $('.header__burger-btn').click(function () {
   $('.main-index__wrap').toggleClass('main-index__wrap_active');
+  $('.main-index').toggleClass('main-index_active');
   $('.footer').toggleClass('footer_deactivate');
   $(this).toggleClass('header__burger-btn_active');
+  if ($("div").is(".main-index__wrap_active")) {
+    $("body").css({
+      "overflow": "hidden"
+    });
+  } else {
+    $("body").css({
+      "overflow": "auto"
+    });
+  }
+
 })
--- build/js/custom.js
+++ build/js/custom.js
@@ -3,8 +3,18 @@
 $('document').ready(function () {
   $('.header__burger-btn').click(function () {
     $('.main-index__wrap').toggleClass('main-index__wrap_active');
+    $('.main-index').toggleClass('main-index_active');
     $('.footer').toggleClass('footer_deactivate');
     $(this).toggleClass('header__burger-btn_active');
+    if ($("div").is(".main-index__wrap_active")) {
+      $("body").css({
+        "overflow": "hidden"
+      });
+    } else {
+      $("body").css({
+        "overflow": "auto"
+      });
+    }
   });
   var modalVideo = new Kmodal('.modal-video');
   var modalCovers = new Kmodal('.modal-covers');
